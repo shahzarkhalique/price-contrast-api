3aa2de38ce09ab8c719c8a06035376aeedd89f01
shahzarkhalique <97937180+shahzarkhalique@users.noreply.github.com>
2025-05-05 19:16:54 +0500

    Update config.py

--- App/config.py
+++ App/config.py
@@ -7,4 +7,4 @@ load_dotenv()
 
 # Configuration variables
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_wNTU4WiDqJQ5@ep-square-tooth-a1szbuny-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require")
+DATABASE_URL = os.getenv("DATABASE_URL")
